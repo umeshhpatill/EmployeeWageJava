12641903486c0a96ff41a2271be14728c700dd39
umesh <usp5758@gmail.com>
2020-05-13 22:34:54 +0530

    Add Part Time Employee

---
+++ PartTimeEmpWageUC3.java
@@ -0,0 +1,20 @@
+public class PartTimeEmpWageUC3 {
+     public static void main(String[] args) {
+     int IS_PART_TIME = 1;
+     int IS_FULL_TIME = 1;
+     int EMP_RATE_PER_HOUR = 20;
+
+     int empHrs = 0;
+     int empWage = 0;
+
+     double empCheck = Math.floor(Math.random() * 10) % 3;
+     if (empCheck == IS_FULL_TIME)
+         empHrs = 8;
+     else if (empCheck == IS_PART_TIME)
+         empHrs = 4;
+     empWage = empHrs * EMP_RATE_PER_HOUR;
+     System.out.println("Emp Wage: " + empWage);
+     }
+}
+
+
